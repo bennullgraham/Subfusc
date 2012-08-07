6e4887e08a920211f697bbff68f9e9b7f8256f8a
Ben Graham <bgraham@bgraham.com.au>
2012-08-07 16:33:03 +1000

    Page about my setup for ST2; files for SyntaxHighlighter (guess what /that's/ for).

--- min/groupsConfig.php
+++ min/groupsConfig.php
@@ -12,6 +12,8 @@
 return array(
     'js' => array('//js/profile-links-hover.js'),
     'css' => array('//css/reset.css', '//css/style.css', '//css/font.css', '//css/skeleton.css'),
+    'jssh' => array(/*'//js/syntax/xregexp-min.js',*/ '//js/syntax/shCore.js', '//js/syntax/shBrushCss.js', '//js/syntax/shBrushPhp.js', '//js/syntax/shBrushPython.js', '//js/syntax/shBrushBash.js', '//js/syntax/shBrushJScript.js'),
+    'csssh' => array('//css/syntax/shCore.css', '//css/syntax/shThemeGithub.css'),
 
     // custom source example
     /*'js2' => array(
